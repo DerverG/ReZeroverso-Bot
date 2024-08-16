f04f0028fb927ec5c7b04a73f3dbdbae7bd6d9e3
Derver <juan17nov@hotmail.com>
2024-08-16 02:55:43 -0700

    add index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const {Client, GatewayIntentBits, Partials, Collection} = require('discord.js')
+const config = require('dotenv').config()
+const token = process.env.DISCORD_TOKEN
+
+const client = new Client ({
+    intents: [Object.keys(GatewayIntentBits)],
+    partials: [Object.keys(Partials)],
+})
+
+client.login(token).then((result) => {
+    console.log(`El Bot: ${client.user.tag} esta conectado.`)
+}).catch((err) => {
+    console.log('Ocurrio un error al encender el bot.')
+    console.error(err)
+})
